7a37d6147cb5cf274f6efb9197402295f83758a6
parth578 <parthshukla578@gmail.com>
2023-12-10 14:08:12 +0530

    .env.example changed

--- src/po_handle/settings/base.py
+++ src/po_handle/settings/base.py
@@ -33,7 +33,6 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR, ".env"))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
-print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
